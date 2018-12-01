427169fe88addd26556282e5258a49d7c33b4291
Damien Picard <dam.pic@free.fr>
2018-12-01 17:44:29 +0100

    Player movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,22 +1,28 @@
-extends Node2D
+extends KinematicBody2D
 
-export var velocity = Vector2(10, 10)
-var movement = Vector2()
+export var VELOCITY = Vector2(100, 100)
+var direction = Vector2()
 
 func _ready():
     pass
 
 func _process(delta):
-    position += velocity * delta * movement
+    direction = get_local_mouse_position()
+    if direction.length_squared() < 5:
+        direction.x = 0
+        direction.y = 0
+    direction = direction.normalized()
+#    position += velocity * delta * direction
+    move_and_slide(VELOCITY * direction)
 
-func _unhandled_key_input(event):
-    if event is InputEventKey:
-        if event.pressed:
-            if event.scancode == KEY_LEFT:
-                movement.x = -1
-            if event.scancode == KEY_RIGHT:
-                movement.x = 1
-            if event.scancode == KEY_UP:
-                movement.y = -1
-            if event.scancode == KEY_DOWN:
-                movement.y = 1
+#func _unhandled_key_input(event):
+#    if event is InputEventKey:
+#        if event.pressed:
+#            if event.scancode == KEY_LEFT:
+#                movement.x = -1
+#            if event.scancode == KEY_RIGHT:
+#                movement.x = 1
+#            if event.scancode == KEY_UP:
+#                movement.y = -1
+#            if event.scancode == KEY_DOWN:
+#                movement.y = 1
