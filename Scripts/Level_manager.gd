14783efb9011a189b85779891b24467df5f3afd4
CPau3D <cpau3D@gmail.com>
2018-12-02 21:40:10 +0100

    create level manager

--- Scripts/Level_manager.gd
+++ Scripts/Level_manager.gd
@@ -1,13 +1,15 @@
 extends Node2D
 
-export(Array) var boid_nodes = Array()
+export(Array) var boidArray = Array()
+
+var boid_nodes = Array()
 
 enum BOID_TYPES { RED, YELLOW, BLUE }
 
 const boids = {
-	RED: "res://Boids/Red/",
-	YELLOW: "res://Boids/Yellow/",
-	BLUE: "res://Boids/Blue/",
+	"RED": "res://Boids/Red/",
+	"YELLOW": "res://Boids/Yellow/",
+	"BLUE": "res://Boids/Blue/",
 	}
 
 onready var timer = Timer.new()
@@ -23,9 +25,11 @@ func _ready():
 #	timer.start()
 	add_child(timer)
 	
-	for i in range(0, boid_nodes.size()):
-		for i in range(1,5):
-			var path = boids[boid_nodes] + str(i) + ".tscn"
+	for i in range(0, boidArray.size()):
+		for j in range(1,6):
+			print(boidArray[i])
+			var path = boids[boidArray[i]] + str(j) + ".tscn"
+			print(path)
 			boid_nodes.push_back(load(path))
 	print(boid_nodes)
 #	print(timer.is_stopped())
@@ -33,10 +37,10 @@ func _ready():
 
 func add_boid():
 #	print("Adding boid...")
-#	var boid_type = randi() % boid_nodes.size()
-#	var boid_instance = boid_nodes[boid_type].instance()
-#	var angle = rand_range(-PI, PI)
-#	boid_instance.position = Vector2(cos(angle), sin(angle))*boid_radius
+	var boid_type = randi() % boid_nodes.size()
+	var boid_instance = boid_nodes[boid_type].instance()
+	var angle = rand_range(-PI, PI)
+	boid_instance.position = Vector2(cos(angle), sin(angle))*boid_radius
 #	get_tree().root.add_child(boid_instance)
-#	get_node("/root/Main").add_child(boid_instance)
+	get_node("/root/Main").add_child(boid_instance)
 	pass
