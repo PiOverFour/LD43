59a7c1c031e7e49384965e9ccad792f346b81ec2
Damien Picard <dam.pic@free.fr>
2018-12-03 22:11:35 +0100

    Hud: vertical life bar

---
+++ HUD.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+
+
+func update_lives(sacrificed_boids, total_lives):
+	# Reset all icons to visible
+	for i in range(total_lives):
+		if sacrificed_boids < total_lives:
+			get_node("Lives/Grid/Life" + str(i)).visible = i >= sacrificed_boids
