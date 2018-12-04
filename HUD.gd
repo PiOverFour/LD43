2e78af81803da31a1aa40d4cef07822b37dfcafe
CPau3D <cpau3D@gmail.com>
2018-12-04 01:05:16 +0100

    hud anim

--- HUD.gd
+++ HUD.gd
@@ -5,7 +5,12 @@ func update_lives(sacrificed_boids, total_lives):
 	# Reset all icons to visible
 	for i in range(total_lives):
 		if sacrificed_boids < total_lives:
-			get_node("Lives/Grid/Life" + str(i)).visible = i >= sacrificed_boids
+#			get_node("Lives/Grid/Life" + str(i)).visible = i >= sacrificed_boids
+			if i >= sacrificed_boids:
+				get_node("Lives/Grid/Life" + str(i) + "/AnimationPlayer").assigned_animation = "play"
+				get_node("Lives/Grid/Life" + str(i) + "/AnimationPlayer").seek(0)
+			else:
+				get_node("Lives/Grid/Life" + str(i) + "/AnimationPlayer").play("play")
 
 
 func _on_ContinueButton_pressed():
