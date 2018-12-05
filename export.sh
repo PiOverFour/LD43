cfd0d3ecf4d22817d64c113689d266531580b3c7
Damien Picard <dam.pic@free.fr>
2018-12-06 00:09:23 +0100

    Export: fix HTML

--- export.sh
+++ export.sh
@@ -8,13 +8,19 @@ mkdir -p export/$GAME.HTML/
 #mkdir -p export/$GAME.mac/
 mkdir -p export/$GAME.win/
 
-$GODOT --export-debug "Linux/X11" export/$GAME.lin/$GAME.x86_64
-$GODOT --export-debug "HTML5" export/$GAME.HTML/$GAME.html
-$GODOT --export-debug "Mac OSX" export/$GAME.mac.zip
-$GODOT --export-debug "Windows Desktop" export/$GAME.win/$GAME.exe
+$GODOT --export "Linux/X11" export/$GAME.lin/$GAME.x86_64
+$GODOT --export "HTML5" export/$GAME.HTML/$GAME.html
+$GODOT --export "Mac OSX" export/$GAME.mac.zip
+$GODOT --export "Windows Desktop" export/$GAME.win/$GAME.exe
+
+mv export/$GAME.HTML/$GAME.html export/$GAME.HTML/index.html
 
 cd export
 
 zip -r $GAME.lin.zip $GAME.lin/*
 zip -r $GAME.win.zip $GAME.win/*
-zip -r $GAME.HTML.zip $GAME.HTML/*
+
+# Move to dir so index.html is at the zip's root
+cd $GAME.HTML
+zip -r $GAME.HTML.zip ./*
+mv $GAME.HTML.zip ..
